4d955bb8b3162e338c3c6ec771355fbda2ad41c1
Robert Johansson <robertjo79@gmail.com>
2016-11-15 20:54:51 +0100

    Initial commit

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+
+function parent_css_theme_style() { 
+ wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' ); 
+}
+add_action( 'wp_enqueue_scripts', 'parent_css_theme_style' );
+
+?>
+
